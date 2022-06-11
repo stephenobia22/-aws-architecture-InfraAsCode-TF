fef99fb5fca864ce085e14449640757e25b1613f
Stephen Obia <68517142+stephenobia22@users.noreply.github.com>
2022-06-11 19:05:02 +0100

    Update variables.tf

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,1 +1,12 @@
+# ------ ./modules/networking/variables.tf ------
 
+variable "aws_region" {
+  default = "us-west-2"
+}
+variable "vpc_cidr" {}
+variable "environment" {}
+variable "public_subnets_cidr" {}
+variable "private_subnets_cidr" {}
+variable "availability_zones" {}
+variable "private_db_subnets_cidr" {}
+variable "subnate_group_name" {}
