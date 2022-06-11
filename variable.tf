0b83695a31d9d7d401f2bb654421f2cd603c3e78
Stephen Obia <68517142+stephenobia22@users.noreply.github.com>
2022-06-11 15:13:23 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,9 +1,46 @@
-# --- root/variables.tf ---
+# ------ ./modules/networking/variables.tf ------
+
+# variable "public_subnet_numbers" {
+#     type = map(number)
+
+#     description = "Map of AZ to a number that should be used for public subnet"
+
+#     default = {
+#         "us-west-2a" = 1
+#         "us-west-2b" = 2
+#         # "us-west-2c" = 3
+#         # "us-west-2d" = 4
+#     }
+# }
+
+# variable "private_subnet_numbers" {
+#     type = map(number)
+
+#     description = "Map of AZ to a number that should be used for private subnet"
+
+#     default = {
+#         "us-west-2a" = 3
+#         "us-west-2b" = 4
+#         # "us-west-2c" = 3
+#         # "us-west-2d" = 4
+#     }
+# }
+
+# variable "vpc_cidr" {
+#     type = string
+#     description = "The IP range to use for the VPC"
+#     default  = "10.0.0.0/16"
+# }
+
+
+# variable "infra_env" {
+#     type = string
+#     description = "infrastructure environment"
+# }
 
 variable "aws_region" {
-  default = "us-east-1"
+  default = "us-west-2"
 }
-variable "region" {}
 variable "vpc_cidr" {}
 variable "environment" {}
 variable "public_subnets_cidr" {}
